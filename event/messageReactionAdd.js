411c0d59a9607cc57cb0e76bee1c56758fafa715
Docteur-Jemper <62062343+Docteur-Jemper@users.noreply.github.com>
2021-05-04 13:17:58 +0200

    Delete event directory

--- event/messageReactionAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (bot, messageReaction, user) => {
-  const message = messageReaction.message;
-  const member = message.guild.members.cache.get(user.id);
-  const emoji = messageReaction.emoji.name;
-
-  if (messageReaction.partial) await messageReaction.fetch();
-};
