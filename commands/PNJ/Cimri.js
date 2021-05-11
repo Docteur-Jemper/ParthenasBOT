3d552caf1879f26870d2ff2d4deca6f492ce036f
Docteur-Jemper <62062343+Docteur-Jemper@users.noreply.github.com>
2021-05-11 14:36:38 +0200

    Add files via upload

---
+++ commands/PNJ/Cimri.js
@@ -0,0 +1,223 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-var */
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = async (bot, message, args, data, userInfo) => {
+  message.delete();
+  if (message.channel.id !== "568934603486920704") return message.channel.send("Mais Cimri ne se trouve pas ici ??")
+    .then(msg => msg.delete({ timeout: 5000 }))
+    .catch(console.error);
+  if (!args.length) {
+    if (message.member.roles.cache.get("741290028889538620")) {
+      message.channel.send(":x: Attendez un peu avant de faire cette commande")
+        .then(msg => msg.delete({ timeout: 4000 }))
+        .catch(console.error);
+    } else {
+      message.channel.send(`${message.author} s'adresse à Cimri Para`);
+      const Cimri = new MessageEmbed()
+        .setColor("#27d200")
+        .setTitle("Cimri Para :")
+        .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+        .setDescription("Bien le bonjour ! Puis-je vous aider ?")
+        .setFooter(`(${data.prefix}cimri <compte> \/ <parler>)`);
+      message.channel.send(Cimri);
+      message.member.roles.add("741290028889538620");
+      function Blabla() {
+        message.member.roles.remove("741290028889538620");
+      }
+      setTimeout(Blabla, 60000);
+    }
+    return;
+  }
+  let prix = 2500;
+  if (dbProfile.Race === "Gobelin") prix = 1750;
+  if (dbProfile.Race === "Thérianthrope") prix = 3000;
+  if (args[0] === "compte") {
+    if (userInfo.MONNAIE.monnaieB < 0) {
+      message.channel.send(`${message.author} demande à Cimri Para des informations sur les comptes`);
+      const CimriCompte = new MessageEmbed()
+        .setColor("#27d200")
+        .setTitle("Cimri Para :")
+        .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+        .setDescription("Alors comme ça ouvrir un compte vous intéresse ?")
+        .addField("vous sourit", `${message.author} c'est ça ?`)
+        .addField("vous regarde de haut en bas", "Ouvrir un compte dans ma banque vous permettra de mettre votre argents entre de bonne main. En plus de cela, chaque jours vous toucherai des intérêts")
+        .addField("se frotte les mains", `Cela ne vous coutera que ${prix} pièces pour l'ouverture et vous pourrez y déposer votre fortune.\nÊtes-vous intéressé ?`)
+        .setFooter(`(${data.prefix}cimri <ouvrir>)`);
+      message.channel.send(CimriCompte);
+      return;
+    }
+    message.channel.send(`${message.author} s'adresse à Cimri Para pour consulter son compte`);
+    const CimriCompte2 = new MessageEmbed()
+      .setColor("#27d200")
+      .setTitle("Cimri Para :")
+      .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+      .setDescription("Alors, je vais vérifier ça...")
+      .addField("va chercher les informations banquaire", "Alors alors alors...");
+    if (userInfo.MONNAIE.monnaieB === 0) {
+      CimriCompte2.addField("reviens vers vous", "Bon, vous disposer actuellement d'aucune pièce sur votre compte. Voulez-vous mettre de l'argents ?")
+        .setFooter(`(${data.prefix}cimri <déposer> \/ <parler>)`);
+    } else {
+      CimriCompte2.addField("reviens vers vous", `Bon, vous disposer actuellement de ${userInfo.MONNAIE.monnaieB} pièces sur votre compte. Voulez-vous retirer ou mettre de l'argents ?`)
+        .setFooter(`(${data.prefix}cimri <déposer> \/ <retirer> \/ <parler>)`);
+    }
+    message.channel.send(CimriCompte2);
+    return;
+  }
+  if (args[0] === "ouvrir") {
+    if (userInfo.MONNAIE.monnaieB >= 0) {
+      message.channel.send(":x: Vous disposez déjà d'un compte")
+        .then(msg => msg.delete({ timeout: 4000 }))
+        .catch(console.error);
+      return;
+    }
+    if (userInfo.MONNAIE.monnaie >= prix) {
+      message.channel.send(`${message.author} souhaite ouvrir un compte au près de Cimri Para sortant les ${prix} pièces`);
+      bot.updateUserInfo(member, {
+        "users.$.MONNAIE.monnaie": userInfo.MONNAIE.monnaie - prix,
+        "users.$.MONNAIE.monnaieB": 0
+      });
+      const CimriOuvrir = new MessageEmbed()
+        .setColor("#27d200")
+        .setTitle("Cimri Para :")
+        .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+        .setDescription("Et bien c'est d'accord ! Je vous fait ça !")
+        .addField(`vous sourit prenant les ${prix} pièces`, "Alors alors alors...")
+        .addField("remplit quelque paperasse", "Et voilà, votre compte est désormé ouvert ! Si jamais vous souhaiter retirer ou mettre de l'argent sur celui-ci, il suiffira de venir me voir")
+        .addField("vous sert la main", "Ravi de faire affaire avec vous")
+        .setFooter(`(${data.prefix}cimri <déposer> \/ <compte> \/ <parler>)`);
+      message.channel.send(CimriOuvrir);
+    } else {
+      message.channel.send(`${message.author} souhaite ouvrir un compte au près de Cimri Para`);
+      const CimriOuvrirRater = new MessageEmbed()
+        .setColor("#27d200")
+        .setTitle("Cimri Para :")
+        .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+        .setDescription("Et bien c'est d'accord ! Je vous fait ça !")
+        .addField(`vous sourit mais s'apperçois que vous n'avez pas les ${prix} pièces`, "Euuhhh...")
+        .addField("son sourire se brise", "Mais il me faut l'argents pour ouvrir le compte ! Il vous en manque !")
+        .addField("soupir", "Revenez me voir avec les fonds nécessaire..")
+        .setFooter(`(${data.prefix}cimri <ouvrir> \/ <parler>)`);
+      message.channel.send(CimriOuvrirRater);
+    }
+    return;
+  }
+  if (args[0] === "déposer") {
+    if (userInfo.MONNAIE.monnaieB < 0) {
+      message.channel.send(":x: Vous ne disposez d'aucun compte")
+        .then(msg => msg.delete({ timeout: 4000 }))
+        .catch(console.error);
+      return;
+    }
+    const monnaie = Number(args[1]);
+    if (!monnaie || monnaie<0 || monnaie%1!==0 || args[2]) {
+      return message.channel.send(`:x: Veuillez respecter la syntaxe : \`${data.prefix}cimri déposer <nombre>\``)
+        .then(msg => msg.delete({ timeout: 8000 }))
+        .catch(console.error);
+    }
+    if (userInfo.MONNAIE.monnaie < monnaie) {
+      return message.channel.send(":x: Vous n'avez pas assez d'argent !")
+        .then(msg => msg.delete({ timeout: 8000 }))
+        .catch(console.error);
+    }
+    message.channel.send(`${message.author} demande au près de Cimri Para si il pourrait déposer ${monnaie} pièce`);
+    const BanqueD = new MessageEmbed()
+      .setColor("#27d200")
+      .setTitle("Cimri Para :")
+      .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+      .setDescription("Vous voulez déposer du coup !")
+      .addField("prend les pièces que vous lui donner et les comptes", `${monnaie} pièces !`)
+      .addField("vous sourit", "Je vous met ça sur votre compte")
+      .addField("fait un allez-retour pour déposer les pièces", `Vous êtes donc maintenant à ${userInfo.MONNAIE.monnaieB + monnaie} pièces sur votre compte\nN'hésitez pas à revenir me voir !`)
+      .setFooter(`(${data.prefix}cimri <compte> \/ <parler>)`);
+    message.channel.send(BanqueD);
+    bot.updateUserInfo(member, {
+      "users.$.MONNAIE.monnaie": userInfo.MONNAIE.monnaie - monnaie,
+      "users.$.MONNAIE.monnaieB": userInfo.MONNAIE.monnaieB + monnaie
+    });
+    return;
+  }
+  if (args[0] === "retirer") {
+    if (userInfo.MONNAIE.monnaieB < 0) {
+      message.channel.send(":x: Vous ne disposez d'aucun compte")
+        .then(msg => msg.delete({ timeout: 4000 }))
+        .catch(console.error);
+      return;
+    }
+    const monnaie = Number(args[1]);
+    if (!monnaie || monnaie<0 || monnaie%1!==0 || args[2]) {
+      return message.channel.send(`:x: Veuillez respecter la syntaxe : \`${data.prefix}cimri retirer <nombre>\``)
+        .then(msg => msg.delete({ timeout: 8000 }))
+        .catch(console.error);
+    }
+    if (userInfo.MONNAIE.monnaieB < monnaie) {
+      message.delete({ timeout: 5000 });
+      return message.channel.send(":x: Vous n'avez pas assez d'argent sur votre compte !")
+        .then(msg => msg.delete({ timeout: 8000 }))
+        .catch(console.error);
+    }
+    message.channel.send(`${message.author} veux retirer ${monnaie} pièce au près de Cimri Para`);
+    const BanqueR = new MessageEmbed()
+      .setColor("#27d200")
+      .setTitle("Cimri Para :")
+      .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+      .setDescription("Vous voulez retirer c'est ça ?")
+      .addField("vous sourit", "Je vais vous chercher ça")
+      .addField("va chercher l'argent sur votre compte", `Je vous ramène vos ${monnaie} pièces !`)
+      .setFooter(`(${data.prefix}cimri <compte> \/ <parler>)`);
+    if (userInfo.MONNAIE.monnaieB - monnaie === 0) BanqueR.addField("vous les tend", "Il ne reste plus aucune pièces sur votre compte maintenant\nN'hésitez pas à revenir me voir !");
+    else BanqueR
+      .addField("vous les tend", `Il reste ${userInfo.MONNAIE.monnaieB - monnaie} pièces sur votre compte maintenant\nN'hésitez pas à revenir me voir !`);
+    message.channel.send(BanqueR);
+    bot.updateUserInfo(member, {
+      "users.$.MONNAIE.monnaie": userInfo.MONNAIE.monnaie + monnaie,
+      "users.$.MONNAIE.monnaieB": userInfo.MONNAIE.monnaieB - monnaie
+    });
+    return;
+  }
+  if (args[0] === "parler") {
+    const blabla = args[1];
+    if (!blabla) {
+      message.channel.send(`${message.author} s'adresse à Cimri Para souhaitant simplement discuter`);
+      const parlez = new MessageEmbed()
+        .setColor("#27d200")
+        .setTitle("Cimri Para :")
+        .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+        .setDescription("Alors comme ça vous voulez juste parler ?")
+        .addField("sourit légèrement", "Vous avez l'air sympathique")
+        .addField("se gratte la tête", "Je n'est pas grand chose à raconter mais n'hésiter pas à revenir faire la causette avec moi une prochaine fois, j'aurais peut-être quelque chose pour vous")
+        .setFooter(`(${data.prefix}cimri <compte>`);
+      message.channel.send(parlez);
+      return;
+    }
+    if (!args[2]) {
+      if (blabla === "(secret)") {
+        message.channel.send(`${message.author} s'adresse à Cimri Para sur le sujet d'un potenciel secret`);
+        const parlez = new MessageEmbed()
+          .setColor("#27d200")
+          .setTitle("Cimri Para :")
+          .setImage("https://cdn.discordapp.com/attachments/568584015133540382/727125312978288751/Cimri_Para.jpg")
+          .addField("rigole un bon coup", "Alors comme ça tu veux savoir un secret ?")
+          .addField("chuchotte", "Je n'en ai pas ! Mais bien tenté")
+          .setFooter(`(${data.prefix}cimri <compte>`);
+        message.channel.send(parlez);
+        return;
+      }
+    }
+  }
+  message.channel.send(`${message.author} tente de demander quelque chose à Cimri Para`)
+    .then(msg => msg.delete({ timeout: 10000 }))
+    .catch(console.error);
+  message.channel.send("**Cimri Para** : ?? **vous regarde bizarrement n'aillant pas compris**")
+    .then(msg => msg.delete({ timeout: 10000 }))
+    .catch(console.error);
+};
+
+module.exports.help = {
+  name: "cimri",
+  aliases: "",
+  category: "pnj",
+  description: "Il s'agit de la commande pour parler à `Cimri Para`\n(description de se PNJ disponible dans <#568584015133540382>)",
+  cooldown: 6,
+  args: false
+};
---
+++ commands/RP/inventaire.js
@@ -0,0 +1,143 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-var */
+/* eslint-disable block-scoped-var */
+const { MessageEmbed } = require("discord.js");
+const MOB = require("../../assest/Mob/nature");
+
+module.exports.run = async (bot, message, args, data, userInfo) => {
+  message.delete();
+  if (message.channel.id !== "569098535015022612") {
+    message.channel.send("Merci d'executer cette commande dans le bon channel (<#569098535015022612>)")
+      .then(msg => msg.delete({ timeout: 6000 }))
+      .catch(console.error);
+    return;
+  }
+  
+  InventaireLIST=[
+    [userInfo.INVENTAIRE.inventaire.Peau,1,0],
+    [userInfo.INVENTAIRE.inventaire.Griffe,1,1],
+    [userInfo.INVENTAIRE.inventaire.Viande,2,0],
+    [userInfo.INVENTAIRE.inventaire.Bois,2,1],
+    [userInfo.INVENTAIRE.inventaire.Sabot,3,0],
+    [userInfo.INVENTAIRE.inventaire.Dent,5,0],
+    [userInfo.INVENTAIRE.inventaire.Ecaille,5,1],
+    [userInfo.INVENTAIRE.inventaire.Carapace,7,0],
+    [userInfo.INVENTAIRE.inventaire.Mandibule,7,1],
+    [userInfo.INVENTAIRE.inventaire.Crâne,8,0],
+    [userInfo.INVENTAIRE.inventaire.Sang,8,1],
+    [userInfo.INVENTAIRE.inventaire.Aile,9,0],
+    [userInfo.INVENTAIRE.inventaire.Oreille,9,1],
+    [userInfo.INVENTAIRE.inventaire.Corne,10,0],
+    [userInfo.INVENTAIRE.inventaire.Oeil,10,1],
+    [userInfo.INVENTAIRE.inventaire.Pollen,3,1],
+    [userInfo.INVENTAIRE.inventaire.HerbeMedic,4,1],
+    [userInfo.INVENTAIRE.inventaire.Azurite,6,0],
+    [userInfo.INVENTAIRE.inventaire.Diamant,6,1]
+  ]
+  let inventory = "--------------------";
+  for (i=0 ; i<InventaireLIST.length ; i=i+1){
+    if (InventaireLIST[i][0] > 0) {
+      inventory = `${inventory}\n${MOB[InventaireLIST[i][1]].loot[InventaireLIST[i][2]][0]}  ${InventaireLIST[i][0]}`;
+    }
+  }
+  // équipement !
+  const équip = `Armure : ${userInfo.INVENTAIRE.équipement.Armure}\nArme : ${userInfo.INVENTAIRE.équipement.Arme}\nTalisman : ${userInfo.INVENTAIRE.équipement.Talisman}`;
+  /*// inventaire !
+  let inventory = "--------------------";
+  // 1
+  if (userInfo.INVENTAIRE.inventaire.Peau > 0) {
+    inventory = `${inventory}\n<:Peau_de_loup:694189902702837800> Peau de Loup : ${userInfo.INVENTAIRE.inventaire.Peau}`;
+  }
+  // 2
+  if (userInfo.INVENTAIRE.inventaire.Griffe > 0) {
+    inventory = `${inventory}\n<:Griffe_de_loup:694189763590094891> Griffe de Loup : ${userInfo.INVENTAIRE.inventaire.Griffe}`;
+  }
+  // 3
+  if (userInfo.INVENTAIRE.inventaire.Viande > 0) {
+    inventory = `${inventory}\n<:Viande_de_cerf:694210828462719006> Viande de Cerf : ${userInfo.INVENTAIRE.inventaire.Viande}`;
+  }
+  // 4
+  if (userInfo.INVENTAIRE.inventaire.Bois > 0) {
+    inventory = `${inventory}\n<:Bois_de_cerf:694208793013256292> Bois de Cerf : ${userInfo.INVENTAIRE.inventaire.Bois}`;
+  }
+  // 5
+  if (userInfo.INVENTAIRE.inventaire.Sabot > 0) {
+    inventory = `${inventory}\n<:Sabot_de_Pripom:694236178911592458> Sabot de Pripom : ${userInfo.INVENTAIRE.inventaire.Sabot}`;
+  }
+  // 6
+  if (userInfo.INVENTAIRE.inventaire.Dent > 0) {
+    inventory = `${inventory}\n<:Dent_de_croco:694510570732388432> Dent de Croco: ${userInfo.INVENTAIRE.inventaire.Dent}`;
+  }
+  // 7
+  if (userInfo.INVENTAIRE.inventaire.Ecaille > 0) {
+    inventory = `${inventory}\n<:ecaille_de_croco:694506268022538370> Ecaille de Croco : ${userInfo.INVENTAIRE.inventaire.Ecaille}`;
+  }
+  // 8
+  if (userInfo.INVENTAIRE.inventaire.Carapace > 0) {
+    inventory = `${inventory}\n<:Carapace_de_Gimille:694518245801328751> Carapace de Gimille : ${userInfo.INVENTAIRE.inventaire.Carapace}`;
+  }
+  // 9
+  if (userInfo.INVENTAIRE.inventaire.Mandibule > 0) {
+    inventory = `${inventory}\n<:Mandibule_de_Gimille:694522865416011816> Mandibule de Gimille : ${userInfo.INVENTAIRE.inventaire.Mandibule}`;
+  }
+  // 10
+  if (userInfo.INVENTAIRE.inventaire.Crâne > 0) {
+    inventory = `${inventory}\n<:Crane_de_Wretch:694566544885284984> Crâne de Wretch : ${userInfo.INVENTAIRE.inventaire.Crâne}`;
+  }
+  // 11
+  if (userInfo.INVENTAIRE.inventaire.Sang > 0) {
+    inventory = `${inventory}\n<:Fiole_de_sang_de_Wretch:694566605451034645> Fiole de Sang de Wretch : ${userInfo.INVENTAIRE.inventaire.Sang}`;
+  }
+  // 12
+  if (userInfo.INVENTAIRE.inventaire.Aile > 0) {
+    inventory = `${inventory}\n<:Aile_de_Varghulf:694566725496078337> Aile de Varghulf : ${userInfo.INVENTAIRE.inventaire.Aile}`;
+  }
+  // 13
+  if (userInfo.INVENTAIRE.inventaire.Oreille > 0) {
+    inventory = `${inventory}\n<:Oreille_de_Varghulf:694566767284191262> Oreille de Varghulf : ${userInfo.INVENTAIRE.inventaire.Oreille}`;
+  }
+  // 14
+  if (userInfo.INVENTAIRE.inventaire.Corne > 0) {
+    inventory = `${inventory}\n<:Corne_de_Minotaur:694566793213247578> Corne de Minotaur : ${userInfo.INVENTAIRE.inventaire.Corne}`;
+  }
+  // 15
+  if (userInfo.INVENTAIRE.inventaire.Oeil > 0) {
+    inventory = `${inventory}\n<:Oeil_de_Minotaur:694566836754448455> Oeil de Minotaur : ${userInfo.INVENTAIRE.inventaire.Oeil}`;
+  }
+  // 16
+  if (userInfo.INVENTAIRE.inventaire.Pollen > 0) {
+    inventory = `${inventory}\n<:Pollen_de_Pripom:694236079498461284> Pollen de Pripom : ${userInfo.INVENTAIRE.inventaire.Pollen}`;
+  }
+  // 17
+  if (userInfo.INVENTAIRE.inventaire.HerbeMedic > 0) {
+    inventory = `${inventory}\n<:Herbe_Medicinal:694502875929247835> Herbe Medicinal : ${userInfo.INVENTAIRE.inventaire.HerbeMedic}`;
+  }
+  // 18
+  if (userInfo.INVENTAIRE.inventaire.Azurite > 0) {
+    inventory = `${inventory}\n<:Pierre_dazurite:694498949603786792> Pierre d'Azurite : ${userInfo.INVENTAIRE.inventaire.Azurite}`;
+  }
+  // 19
+  if (userInfo.INVENTAIRE.inventaire.Diamant > 0) {
+    inventory = `${inventory}\n<:Diamant:694513788275130489> Diamant : ${userInfo.INVENTAIRE.inventaire.Diamant}`;
+  }*/
+
+  const InvEmbeb = new MessageEmbed()
+    .setColor("#00bfff")
+    .setDescription(`Inventaire de **${message.member.displayName}**`)
+    .setThumbnail(message.author.avatarURL())
+    .addField("Équipement", équip)
+    .addField("Force", `${userInfo.EXP.forceActu} / ${userInfo.EXP.force}`, true)
+    .addField("Pv", `${userInfo.EXP.PvActu} / ${userInfo.EXP.Pv}`, true)
+    .addField("Objets", inventory)
+    .setTimestamp();
+  message.channel.send(InvEmbeb);
+};
+
+module.exports.help = {
+  name: "inventaire",
+  aliases: ["inventory", "inv"],
+  category: "rp",
+  description: "Renvoie votre inventaire (équipement + PV/FORCE + objet).\n**(seulement dans <#569098535015022612>)**",
+  cooldown: 10,
+  args: false
+};
